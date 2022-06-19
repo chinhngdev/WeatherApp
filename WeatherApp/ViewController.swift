6c956d536eeaf0b399f4b285bfea8b3fe63b3c2c
Chinh <22429215+chinhngdev@users.noreply.github.com>
2022-06-19 12:50:31 +0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Chinh Ng on 6/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
